924db67f49a85d5c430b7c52d325499b5f76718e
brunobossomartins1@gmail.com <brunobossomartins1@gmail.com>
2024-11-19 21:08:04 -0300

    finalizada implementacao do botao menu para mobile

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -2,14 +2,20 @@ import outsideClick from "./outsideclick.js";
 export default function initDropDownMenu() {}
 
 const dropdownMenus = document.querySelectorAll("[data-dropdown]");
+const dropInicial = document.querySelector(".drop");
 const seta = document.querySelector("#seta");
+
+// Adiciona o evento de clique
+dropInicial.addEventListener("click", function (event) {
+  event.preventDefault();
+});
 
 dropdownMenus.forEach((menu) => {
   function activateMenu() {
     menu.classList.add("active");
     seta.classList.add("rotar");
   }
-  function deactivateMenu() {
+  function desactivateMenu() {
     menu.classList.remove("active", "clicked");
     seta.classList.remove("rotar");
   }
@@ -17,13 +23,13 @@ dropdownMenus.forEach((menu) => {
   menu.addEventListener("mouseover", activateMenu);
   menu.addEventListener("mouseleave", () => {
     if (!menu.classList.contains("clicked")) {
-      deactivateMenu();
+      desactivateMenu();
     }
   });
 
   menu.addEventListener("click", (event) => {
-    menu.classList.add("clicked");
+    menu.classList.toggle("clicked");
     activateMenu();
-    outsideClick(menu, deactivateMenu);
+    outsideClick(menu, desactivateMenu);
   });
 });
