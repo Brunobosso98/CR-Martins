fd42328678177e393871022f113c4c1b60b77e97
brunobossomartins1@gmail.com <brunobossomartins1@gmail.com>
2024-12-03 20:43:12 -0300

    removido slider do script.js e adicionado arquivo de slider apenas na pagina principal

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ import initHeader from "./modules/header.js";
 import initDropDownMenu from "./modules/dropdown-menu.js";
 import MenuMobile from "./modules/menu-mobile.js";
 import slideVitrine from "./modules/slide.js";
-import initSlider from "./modules/slider.js";
+// import initSlider from "./modules/slider.js";
 import initSlideGaleria from "./modules/galeria.js";
 
 const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
@@ -10,5 +10,5 @@ menuMobile.init();
 
 initDropDownMenu();
 initHeader();
-initSlider();
+// initSlider();
 initSlideGaleria();
